20baac830ee53a71206bf595d87d8cb94fea48bf
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-02-04 21:03:42 -0600

    add recursive function to prompt guesses

--- index.js
+++ index.js
@@ -7,13 +7,34 @@ const inq = require("inquirer");
 console.log("index.js is loaded");
 
 const words = ["apple", "orange", "grapes", "banana", "peach", "watermelon", "cherry", "pineapple", "durian", "plum"]
-// let chooseWord = function(){ 
-//      let randomIndex = Math.floor(words.length * Math.random())
-//      let randomWord = new Word (words[randomIndex], false);  
-//      words.splice(randomIndex,1)
-// }
 
 
+let promptGuess = function(word){
+    if (lives > 0) {
+        inq.prompt([{
+            type: "input",
+            name: "attempt",
+            message: `${word.concat()}\nyour guess (one letter please): `,
+            validate: function validateGuess(name) {
+                return isLetter(name) && name.length === 1;
+            }
+        }]).then(a => {
+            let isCorrect = word.guess(a.attempt);
+            console.log(isCorrect)
+            if (!isCorrect) {
+                lives--;
+                wrongGuesses.push(a.attempt);
+            }
+            console.log("lives: "+lives);
+            console.log("wrongGusses: " + wrongGuesses)
+            console.log(word.concat());
+            promptGuess(word)
+
+        })
+    } else {
+        console.log("too many failed attempts")
+    }
+}
 let wrongGuesses = [];
 let lives = 5;
 
@@ -26,29 +47,29 @@ inq.prompt([{
         console.log("ready");
         let randomIndex = Math.floor(words.length * Math.random())
         let randomWord = new Word(words[randomIndex], false);
+        words.splice(randomIndex,1)
+        promptGuess(randomWord);
+        // if (lives > 0) {
+        //     inq.prompt([{
+        //         type: "input",
+        //         name: "attempt",
+        //         message: `${randomWord.concat()}\nyour guess (one letter please): `,
+        //         validate: function validateGuess(name) {
+        //             return isLetter(name) && name.length === 1;
+        //         }
+        //     }]).then(a => {
+        //         let isCorrect = randomWord.guess(a.attempt);
+        //         console.log(isCorrect)
+        //         if (!isCorrect) {
+        //             lives--;
+        //             wrongGuesses.push(a.attempt);
+        //         }
+        //         console.log("lives: "+lives);
+        //         console.log("wrongGusses: " + wrongGuesses)
+        //         console.log(randomWord.concat());
 
-        if (lives > 0) {
-            inq.prompt([{
-                type: "input",
-                name: "attempt",
-                message: `${randomWord.concat()}\nyour guess (one letter please): `,
-                validate: function validateGuess(name) {
-                    return isLetter(name) && name.length === 1;
-                }
-            }]).then(a => {
-                let isCorrect = randomWord.guess(a.attempt);
-                console.log(isCorrect)
-                if (!isCorrect) {
-                    lives--;
-                    wrongGuesses.push(a.attempt);
-                }
-                console.log("lives: "+lives);
-                console.log("wrongGusses: " + wrongGuesses)
-                console.log(randomWord.concat());
-
-
-            })
-        }
+        //     })
+        // }
 
 
     } else {
