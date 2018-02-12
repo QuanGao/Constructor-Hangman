5a4c4cbb655306ebdde820d689d1f478bbe03486
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-02-12 00:00:44 -0600

    adjust spacing

--- index.js
+++ index.js
@@ -25,30 +25,25 @@ let promptGuess = function(word,words){
                 return isLetter(name) && name.length === 1 && allGuesses.indexOf(name.toLowerCase()) === -1;
             }
         }]).then(a => {
-            let isCorrect = word.guess(a.attempt.toLowerCase());
-            allGuesses.push(a.attempt)
+            let attempt = a.attempt.toLowerCase()
+            let isCorrect = word.guess(attempt);
+            allGuesses.push(attempt)
             if (!isCorrect) {
                 lives--;
                 wrongGuesses.push(a.attempt.toLowerCase());
-                console.log(`Incorrect! Remaining guesses: ${lives}`)
+                console.log(`\nIncorrect! Remaining guesses: ${lives}`)
             } else {
-                console.log("correct!")
+                console.log("\ncorrect!")
             }
             if(wrongGuesses.length>0){
-                console.log("wrongGusses: " + wrongGuesses)
+                console.log("\nwrongGusses: " + wrongGuesses)
             }
-            console.log(`${word.concat()}\n`);
+            console.log(`\n${word.concat()}\n`);
             if(word.answer === word.concat()){
                 wins++;
-                reset();
-                if(words.length > 0){
-                    console.log("you got it! Next word!")
-                    playGame(words);
-                } else {
-                    console.log("you've finish all the words");
-                    console.log(`wins: ${wins} | losses:${losses}`);
-                    startGame()
-                }
+                console.log("You got it!")
+                nextWord(words);
+                
                 
             } else {
                 promptGuess(word,words)
@@ -57,31 +52,31 @@ let promptGuess = function(word,words){
         })
     } else {
         losses ++;
-        reset();
-        if(words.length > 0){
-            console.log(`You lose! The word is ${word.answer}. Next word!`);
-            playGame(words);
-        } else {
-            console.log("you've finish all the words");
-            console.log(`wins: ${wins} | losses:${losses}`);
-            startGame()
-
-        }
+        console.log(`\nYou lose! The word is ${word.answer}.`)
+        nextWord(words);
       
     }
 }
 
-let reset = function(){
+let nextWord = function(words){
     allGuesses = [];
     wrongGuesses = [];
     lives = 5;
+    if(words.length > 0){
+        console.log("\nNext word!")
+        playGame(words);
+    } else {
+        console.log("\nyou've finish all the words");
+        console.log(`\nwins: ${wins} | losses:${losses}`);
+        startGame()
+    }
 }
 
 let playGame = function(words){   
     let randomIndex = Math.floor(words.length * Math.random())
     let randomWord = new Word(words[randomIndex], false);
     words.splice(randomIndex,1)
-    console.log(`${randomWord.concat()}\n`);
+    console.log(`\n${randomWord.concat()}\n`);
     promptGuess(randomWord,words);    
 }
 
@@ -92,17 +87,19 @@ let startGame = function(){
         message: "What level would you like to play?",
         choices: ["easy - 5 fruit pastries", "medium - 5 Game of Thrones characters", "hard - 5 Harry Potter magic spells", "cancel"]
     }]).then(a => {
-        let level = a.choice;
-        console.log(level)
-        if(level === "easy - 5 fruit pastries"){
-            playGame(easyWords);
-        } else if (level === "medium - 5 Game of Thrones characters"){
+        switch(a.choice){
+            case "easy - 5 fruit pastries":
+                playGame(easyWords);
+                break;
+            case "medium - 5 Game of Thrones characters":
             playGame(mediumWords)
-        } else if (level === "hard - 5 Harry Potter magic spells"){
+                break;
+            case "hard - 5 Harry Potter magic spells":
             playGame(hardWords);
-        } else {
-            console.log("next time!")
-            process.exit(-1)
+                break;
+            default:
+                console.log("\nNext Time!\n")
+                process.exit(-1)      
         }
     })
 
